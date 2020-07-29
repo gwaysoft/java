028f45fe6a969041cca6ed0e2c4297d6f224d7d6
gwaysoft <gwaysoft@outlook.com>
2020-07-29 12:15:54 +0800

    add

---
+++ spring.io/src/main/java/com/gwaysoft/dockercomposedemo01/controller/HitController.java
@@ -0,0 +1,24 @@
+package com.gwaysoft.dockercomposedemo01.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HitController {
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    @GetMapping("/")
+    public String index() {
+        return "hello hit index !!!";
+    }
+
+    @GetMapping("hit")
+    public String hit() {
+        long view = redisTemplate.opsForValue().increment("views");
+        return "Hello, hits: " + view;
+    }
+}
