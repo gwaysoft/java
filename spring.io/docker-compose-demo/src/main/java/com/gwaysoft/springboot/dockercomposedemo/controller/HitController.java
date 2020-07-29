64ae2de42ce55b721b77623cda134b44542eb8bd
gwaysoft <gwaysoft@outlook.com>
2020-07-29 12:34:23 +0800

    done

--- spring.io/docker-compose-demo/src/main/java/com/gwaysoft/springboot/dockercomposedemo/controller/HitController.java
+++ spring.io/docker-compose-demo/src/main/java/com/gwaysoft/springboot/dockercomposedemo/controller/HitController.java
@@ -1,4 +1,4 @@
-package com.gwaysoft.dockercomposedemo01.controller;
+package com.gwaysoft.springboot.dockercomposedemo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
