bd75567fec3935889bc0bc8322986ba5764b4581
gwaysoft <gwaysoft@outlook.com>
2020-07-29 11:56:43 +0800

    first

---
+++ spring.io/src/test/java/com/gwaysoft/dockercomposedemo01/DockerComposeDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwaysoft.dockercomposedemo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerComposeDemo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
